9e5a70ef8fa11ea627a97d7cf473257023bb2642
Kate Raskauskas <kateapault@gmail.com>
2020-03-19 14:17:49 -0400

    created elements and set up nav and pagecontainer within app

---
+++ src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function Nav() {
+    return (
+      <div id="nav">
+        NAV YEAH
+      </div>
+    );
+  }
+  
+export default Nav;
---
+++ src/PageContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class PageContainer extends React.Component {
+    render() {
+        return (
+        <div id="PageContainer">
+            PC YEAH
+        </div>
+        );
+    }
+}
+  
+  export default PageContainer;

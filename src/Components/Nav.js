5bb4c439f681b2972baa26f0fcc9b95fb626cdc3
github username <github email address>
2020-03-26 15:33:37 -0400

    nav buttons won't show until logged in

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,18 +2,18 @@ import React from 'react';
 import {BrowserRouter as Router, Link} from 'react-router-dom'
 
 function Nav(props) {
-    return (
-      <div id="nav">
-        <h2>Name{/*props.user.name*/}</h2><br></br><br></br>
-        <button className='nav-button' onClick={props.handleNavClick}><div><Link to="/projects" name="projects">My Projects</Link></div></button><br></br><br></br><br></br>
-        <button className='nav-button' onClick={props.handleNavClick}><div><Link to="/schedule" name="schedule">My Schedule</Link></div></button><br></br><br></br><br></br>
-        <br></br><br></br>
-        {/* <button className='nav-button' onClick={props.handleNavClick}><div><Link to="/followers" name="followers">Followers</Link></div></button><br></br><br></br><br></br> */}
-        {/* <button className='nav-button' onClick={props.handleNavClick}><div><Link to="/following" name="following">Following</Link></div></button><br></br><br></br><br></br><br></br><br></br> */}
-        <button className='nav-button' onClick={props.handleNavClick}><div><Link to="/search" name="search">Search GitHub</Link></div></button><br></br><br></br><br></br><br></br><br></br>
-        <button className='nav-button' onClick={props.logOut}>Log Out</button>
-      </div>
-    );
-  }
+  return (
+    <div id="nav">
+      {props.loggedIn && <div><h2>{props.user.name}</h2><br></br><br></br>
+      <button className='nav-button' onClick={props.handleNavClick}><div><Link to="/projects" name="projects">My Projects</Link></div></button><br></br><br></br><br></br>
+      <button className='nav-button' onClick={props.handleNavClick}><div><Link to="/schedule" name="schedule">My Schedule</Link></div></button><br></br><br></br><br></br>
+      <br></br><br></br>
+      {/* <button className='nav-button' onClick={props.handleNavClick}><div><Link to="/followers" name="followers">Followers</Link></div></button><br></br><br></br><br></br> */}
+      {/* <button className='nav-button' onClick={props.handleNavClick}><div><Link to="/following" name="following">Following</Link></div></button><br></br><br></br><br></br><br></br><br></br> */}
+      <button className='nav-button' onClick={props.handleNavClick}><div><Link to="/search" name="search">Search GitHub</Link></div></button><br></br><br></br><br></br><br></br><br></br>
+      <button className='nav-button' onClick={props.logOut}>Log Out</button></div>}
+    </div>
+  );
+}
   
 export default Nav;
